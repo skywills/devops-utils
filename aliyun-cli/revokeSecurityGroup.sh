8c4b2ebe043f583e991ace2573e9b6e381048297
williamkhoo <william_khoo@outlook.com>
2023-02-11 00:02:05 +0800

    add output check

--- aliyun-cli/revokeSecurityGroup.sh
+++ aliyun-cli/revokeSecurityGroup.sh
@@ -2,10 +2,14 @@ OLD_IP=127.0.0.1
 GROUP_ID=sg-j6cj4w5enmbfl6hkh2f4
 PORT_RANGE=5335/5335
 PERMISSIONS=$(aliyun ecs DescribeSecurityGroupAttribute --SecurityGroupId=$GROUP_ID | jq --arg OLD_IP "$OLD_IP" --arg PORT_RANGE "$PORT_RANGE" '[.Permissions.Permission[] | select(.SourceCidrIp == $OLD_IP and .PortRange == $PORT_RANGE)]')
-for PERMISSION in $(echo "$PERMISSIONS" | jq -c '.[]'); do
-  SOURCE_CIDR_IP=$(echo $PERMISSION | jq -r '.SourceCidrIp')
-  PORT_RANGE=$(echo $PERMISSION | jq -r '.PortRange')
-  PROTOCOL=$(echo $PERMISSION | jq -r '.IpProtocol')
-  echo "Revoke Source CIDR IP: $SOURCE_CIDR_IP, Port Range: $PORT_RANGE, IP Protocol: $PROTOCOL"
-  aliyun ecs RevokeSecurityGroup --SecurityGroupId=$GROUP_ID --IpProtocol=$PROTOCOL --PortRange=$PORT_RANGE --SourceCidrIp=$SOURCE_CIDR_IP
-done
+if [ $(echo $PERMISSIONS | jq type) = "array" ]; then
+  for PERMISSION in $(echo "$PERMISSIONS" | jq -c '.[]'); do
+    SOURCE_CIDR_IP=$(echo $PERMISSION | jq -r '.SourceCidrIp')
+    PORT_RANGE=$(echo $PERMISSION | jq -r '.PortRange')
+    PROTOCOL=$(echo $PERMISSION | jq -r '.IpProtocol')
+    echo "Revoke Source CIDR IP: $SOURCE_CIDR_IP, Port Range: $PORT_RANGE, IP Protocol: $PROTOCOL"
+    aliyun ecs RevokeSecurityGroup --SecurityGroupId=$GROUP_ID --IpProtocol=$PROTOCOL --PortRange=$PORT_RANGE --SourceCidrIp=$SOURCE_CIDR_IP
+  done
+else
+  echo "No matching permissions found."
+fi  
