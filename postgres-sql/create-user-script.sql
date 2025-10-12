85450012a3933b0c5767daae3e873399e545d803
williamkhoo <william_khoo@outlook.com>
2025-10-12 12:14:36 +0800

    update create user and grant

---
+++ postgres-sql/create-user-script.sql
@@ -0,0 +1,9 @@
+-- Replace 'mydatabase', 'myuser', and 'mypassword' with your desired values
+
+CREATE DATABASE mydatabase;
+
+CREATE USER myuser WITH PASSWORD 'mypassword';
+
+GRANT ALL PRIVILEGES ON DATABASE mydatabase TO myuser;
+
+GRANT ALL PRIVILEGES ON SCHEMA public TO myuser;
